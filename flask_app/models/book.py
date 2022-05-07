866364ae61a3a1847000a7fc0724f624e2880415
Meisha Booker <meisha.booker@comcast.net>
2022-05-06 20:47:38 -0700

    updated book.py

--- flask_app/models/book.py
+++ flask_app/models/book.py
@@ -1,25 +1,59 @@
 from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app)  
 from flask import flash
-
+from flask_app.config.mysqlconnection import connectToMySQL
 class Book:
+    db_name = 'solo_project'
     
+    def __init__(self, db_data):
+        self.id = db_data['id']
+        self.title = db_data['title']
+        self.author = db_data['author']
+        self.release_year = db_data['release_year']
+        self.created_at = db_data['createdAt']
+        self.updated_at = db_data['updatedAt']
+
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO book (title, author, release_year, under_30, date_made, user_id, createdAt, updatedAt) VALUES (%(title)s, %(author)s, %(release_year)s, %(user_id)s, NOW(), NOW());"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM book;"
+        results =  connectToMySQL(cls.db_name).query_db(query)
+        all_books = []
+        for row in results:
+            print(row['release_year'])
+            all_books.append(cls(row))
+        return all_books
+
+    @classmethod
+    def get_one(cls,data):
+        query = "SELECT * FROM book WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db_name).query_db(query,data)
+        return cls(results[0])
+
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE book SET title=%(title)s, author=%(author)s, release_year=%(release_year)s, updatedAt=NOW() WHERE id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
     
-    
-    
+    @classmethod
+    def delete(cls,data):
+        query = "DELETE FROM book WHERE id = %(id)s;"
+        return connectToMySQL(cls.db_name).query_db(query,data)
+
     @staticmethod
-    def validate_burger(burger):
-        is_valid = True 
-        if len(burger['name']) < 3:
-            flash("Name must be at least 3 characters.")
+    def validate_book(book):
+        is_valid = True
+        if len(book['title']) < 2:
             is_valid = False
-        if len(burger['bun']) < 3:
-            flash("Bun must be at least 3 characters.")
+            flash("Title must be at least 2 characters","book")
+        if len(book['author']) < 2:
             is_valid = False
-        if int(burger['calories']) < 200:
-            flash("Calories must be 200 or greater.")
+            flash("Author must be at least 2 characters","book")
+        if book['release_year'] == "":
             is_valid = False
-        if len(burger['meat']) < 3:
-            flash("Bun must be at least 3 characters.")
-            is_valid = False
+            flash("Please enter a release year","book")
         return is_valid
