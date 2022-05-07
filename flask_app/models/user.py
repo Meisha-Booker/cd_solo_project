e62da8f19336a3d65fac0f8475a381c59ae87ea7
Meisha Booker <meisha.booker@comcast.net>
2022-05-06 20:33:04 -0700

    updated user.py

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,17 +1,66 @@
+from flask_app.config.mysqlconnection import connectToMySQL
 from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app)  
 import re	
  
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 class User:
-    
+    db = "solo_project"
+    def __init__(self,data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.createdAt = data['createdAt']
+        self.updatedAt = data['updatedAt']
 
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM user;"
+        results = connectToMySQL(cls.db_name).query_db(query)
+        users = []
+        for row in results:
+            users.append(cls(row))
+        return users
 
+    @classmethod
+    def save(cls,data):
+        query = "INSERT INTO user (first_name,last_name,email,password) VALUES(%(first_name)s,%(last_name)s,%(email)s,%(password)s)"
+        return connectToMySQL(cls.db).query_db(query,data)
+
+    @classmethod
+    def get_by_email(cls,data):
+        query = "SELECT * FROM user WHERE email = %(email)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        if len(results) < 1:
+            return False
+        return cls(results[0])
+
+    @classmethod
+    def get_by_id(cls,data):
+        query = "SELECT * FROM user WHERE id = %(id)s;"
+        results = connectToMySQL(cls.db).query_db(query,data)
+        return cls(results[0])
 
     @staticmethod
-    def validate_user( user ):
+    def validate_register(chef):
         is_valid = True
-        if not EMAIL_REGEX.match(user['email']): 
-            flash("Invalid email address!")
-            is_valid = False
+        query = "SELECT * FROM user WHERE email = %(email)s;"
+        results = connectToMySQL(User.db).query_db(query,chef)
+    
+        if not EMAIL_REGEX.match(chef['email']):
+            flash("Invalid Email!!!","create")
+            is_valid=False
+        if len(chef['first_name']) < 2:
+            flash("First name must be at least 2 characters","create")
+            is_valid= False
+        if len(chef['last_name']) < 2:
+            flash("Last name must be at least 2 characters","create")
+            is_valid= False
+        if len(chef['password']) < 8:
+            flash("Password must be at least 8 characters","create")
+            is_valid= False
+        if chef['password'] != chef['confirm']:
+            flash("Passwords don't match","create")
         return is_valid
